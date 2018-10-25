a47d432965e6cac67d3ad70420ff418cde6c122e
Christia T <christiatidwell@bellsouth.net>
2018-10-25 14:56:16 +0000

    automatically backed up by learn

--- app/models/song_genre.rb
+++ app/models/song_genre.rb
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
+
 class SongGenre < ActiveRecord::Base
-  belongs_to :song
-  belongs_to :genre 
-=======
-class Song_Genre < ActiveRecord::Base
   belongs_to :song
   belongs_to :genre
 
->>>>>>> aed896edd94861271a707cb4c782f68caf2bd6e4
 end
